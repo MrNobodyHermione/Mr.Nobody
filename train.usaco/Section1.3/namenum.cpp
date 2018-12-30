ddb0da94da38d3c4c36bba72922a7e21a22f7f96
unknown <1184767942@qq.com>
2018-12-30 13:16:51 +0800

    test

---
+++ train.usaco/Section1.3/namenum.cpp
@@ -0,0 +1,31 @@
+/*
+ID: hjl11841
+TASK: namenum
+LANG: C++
+*/
+#include<bits/stdc++.h>
+using namespace std;
+string name[5000];
+char ft[10][4]={{},{},"ABC","DEF","GHI","JKL","MNO","PRS","TUV","WXY"};
+int main(){
+    freopen("dict.txt","r",stdin);
+    freopen("namenum.in","r",stdin);
+    freopen("namenum.out","w",stdout);
+    int n=0;
+    while(cin>>name[n]) n++;
+    string x=name[0];
+    n--;
+    bool none=1;
+    for(int i=1;i<=n;i++){
+        bool tmp=(name[i].size()==x.size());
+        for(int j=0;j<name[i].size();j++){
+            char c=name[i][j];
+            tmp=tmp&(c==ft[x[j]-'0'][0]||c==ft[x[j]-'0'][1]||c==ft[x[j]-'0'][2]);
+        }
+        if(tmp){
+            cout<<name[i].c_str()<<endl;
+            none=0;
+        }
+    }
+    if(none) cout<<"NONE"<<endl;
+}
