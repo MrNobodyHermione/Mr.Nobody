c2441452645382d8ac2e00f0f4f9a12f515eaa16
MrNobodyHermione <45703940+MrNobodyHermione@users.noreply.github.com>
2019-01-20 12:56:06 +0000

    Create milk2.cpp

---
+++ train.usaco/Section1.3/milk2.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int c[1000002]={0};
+int main(){
+    int start=2e9,end=-2e9;
+    int n;
+    int a,b;
+    int tf=1;
+    int t[2]={0};
+    int nstart;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        c[a]++;
+        c[b]--;
+        start=min(start,a);
+        end=max(end,b-1);
+    }
+    nstart=start;
+    int nc;
+    end++;
+    for(int i=start;i<=end;i++){
+        c[i]=c[i-1]+c[i];
+        nc=c[i]==0?0:1;
+        if(nc!=tf||i==end){
+            t[tf]=max(t[tf],i-nstart);
+            nstart=i;
+            tf=1-tf;
+        }
+    }
+    cout<<t[1]<<' '<<t[0]<<endl;
+    return 0;
+}
